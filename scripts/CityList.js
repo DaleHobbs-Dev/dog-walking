2ad36de1bf313dc5e54ccafab9a47c8eb39a3baf
Dale Hobbs <dale.hobbs.dev@gmail.com>
2025-08-16 13:43:17 -0500

    added click events to the three columns

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -2,11 +2,22 @@ import { getWalkers } from "./database.js"
 
 const walkers = getWalkers()
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const clickedTarget = clickEvent.target;
+
+        if (clickedTarget.dataset.type === "city") {
+            window.alert(`The walker servicing this city is ${clickedTarget.dataset.walkername}`)
+        }
+    }
+)
+
 export const CityList = () => {
     let citiesHTML = "<ol>"
 
     for (const walker of walkers) {
-        citiesHTML += `<li>${walker.city}</li>`
+        citiesHTML += `<li data-type="city" data-walkername="${walker.name}">${walker.city}</li>`
     }
 
     citiesHTML += "</ol>"
